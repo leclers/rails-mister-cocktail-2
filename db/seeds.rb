fa48843b77fb083df67266f96373d2d8c233234a
Serge Leclerc <leclercconseil@gmail.com>
2018-11-18 12:19:13 -0500

    Create models and cocktail controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.destroy_all
+# Dose.destroy_all
+# Ingredient.destroy_all
+
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
+mint_leaves = Ingredient.create(name: "mint leaves")
+vodka = Ingredient.create(name: "vodka")
+gin = Ingredient.create(name: "gin")
+rhum = Ingredient.create(name: "rhum")
+
+cocktail1 = Cocktail.create(name: 'manhattan')
+
+# Dose.create(cocktail: cocktail1, ingredient: lemon, description: '1oz')
+# Dose.create(cocktail: cocktail1, ingredient: ice, description: '3 cubes')
